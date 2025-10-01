9f9abce0e622b98d793d6fc044790df37f4bdabe
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2025-10-01 20:15:40 +0200

    cleanup

--- src/main/kotlin/io/wollinger/graves/EntityAccessorHelper.kt
+++ src/main/kotlin/io/wollinger/graves/EntityAccessorHelper.kt
@@ -3,45 +3,49 @@ package io.wollinger.graves
 import io.wollinger.graves.mixin.EntityAccessor
 import net.minecraft.component.type.NbtComponent
 import net.minecraft.entity.Entity
+import net.minecraft.nbt.NbtCompound
 import net.minecraft.nbt.NbtList
 import kotlin.jvm.optionals.getOrNull
 
 object EntityAccessorHelper {
     private fun createKey(key: String) = "SVENS_GRAVES_$key"
 
-    fun writeString(entity: Entity, key: String, data: String) {
+    fun getEditableNbt(entity: Entity, action: (NbtCompound) -> Unit) {
         val accessor = entity as EntityAccessor
         val nbt = accessor.customData.copyNbt()
-        nbt.putString(createKey(key), data)
+        action.invoke(nbt)
         accessor.customData = NbtComponent.of(nbt)
+    }
+
+    fun getNbt(entity: Entity) = (entity as EntityAccessor).customData.copyNbt()
+
+    fun writeString(entity: Entity, key: String, data: String) {
+        getEditableNbt(entity) { nbt ->
+            nbt.putString(createKey(key), data)
+        }
     }
 
     fun writeInt(entity: Entity, key: String, data: Int) {
-        val accessor = entity as EntityAccessor
-        val nbt = accessor.customData.copyNbt()
-        nbt.putInt(createKey(key), data)
-        accessor.customData = NbtComponent.of(nbt)
+        getEditableNbt(entity) { nbt ->
+            nbt.putInt(createKey(key), data)
+        }
     }
 
     fun writeNbtList(entity: Entity, key: String, data: NbtList) {
-        val accessor = entity as EntityAccessor
-        val nbt = accessor.customData.copyNbt()
-        nbt.put(createKey(key), data)
-        accessor.customData = NbtComponent.of(nbt)
+        getEditableNbt(entity) { nbt ->
+            nbt.put(createKey(key), data)
+        }
     }
 
     fun readString(entity: Entity, key: String): String? {
-        val accessor = entity as EntityAccessor
-        return accessor.customData.copyNbt().getString(createKey(key)).getOrNull()
+        return getNbt(entity).getString(createKey(key)).getOrNull()
     }
 
     fun readInt(entity: Entity, key: String): Int? {
-        val accessor = entity as EntityAccessor
-        return accessor.customData.copyNbt().getInt(createKey(key)).getOrNull()
+        return getNbt(entity).getInt(createKey(key)).getOrNull()
     }
 
     fun readNbtList(entity: Entity, key: String): NbtList? {
-        val accessor = entity as EntityAccessor
-        return accessor.customData.copyNbt().getList(createKey(key)).getOrNull()
+        return getNbt(entity).getList(createKey(key)).getOrNull()
     }
 }
